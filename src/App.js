fb266e89afd7eb136dc2c33e065b49e04a7ce241
Nenad Stojkovic <neske.stojkovic@gmail.com>
2019-05-29 22:23:55 +0200

    upload

---
+++ src/App.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function Todo({ todo, index, completeTodo, removeTodo }) {
+  return (
+    <div
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+      className="todo"
+    >
+      {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+        <button onClick={() => removeTodo(index)}>Remove</button>
+      </div>
+    </div>
+  );
+}
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmite = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmite}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        placeholder="Add new todo..."
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "Finish portfolio",
+      isCompleted: false
+    },
+    {
+      text: "Update resume",
+      isCompleted: false
+    },
+    {
+      text: "Upload javascript project",
+      isCompleted: false
+    },
+    {
+      text: "Finish website",
+      isCompleted: true
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
